b4701e50db073e1ad7754d17a9b51452997f6f28
maxwellbenton <maxwellbenton@gmail.com>
2018-12-12 10:40:23 -0500

    removes sqlite db files, updates support files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2015_12_01_201114) do
+ActiveRecord::Schema.define(version: 20151201201114) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
