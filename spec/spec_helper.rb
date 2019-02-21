086aa8eb381fd77a893a4daadddf9c4cd4d6b7d4
Emily Wilder <emilywilder.earth@gmail.com>
2019-02-21 14:41:15 -0500

    ActiveRecord has moved #needs_migration? from ActiveRecord::Migrator to ActiveRecord::base.connection.migration_context in 5.2; however, both <5.2 and >=5.2 have Ac tiveRecord::Migration.check_pending! which uses the correct method and raises an ActiveRecord::PendingMigrationError if there are migrations that need to be performed.
    Remvoved use of needs_migration? in favor of check_pending!
    Moved check to a shared method #fi_check_migration in environment.rb since both rspec and rackup use it
    Rewrote the exception message to use the appropriate rake and env
    Set the SINATRA_ENV to test for rackup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,11 @@ require 'rack/test'
 require 'capybara/rspec'
 require 'capybara/dsl'
 
-if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
-  raise 'Migrations are pending run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
+begin
+  fi_check_migration
+rescue ActiveRecord::PendingMigrationError => err
+  STDERR.puts err
+  exit 1
 end
 
 RSpec.configure do |config|
